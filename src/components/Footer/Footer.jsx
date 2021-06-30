6c2bdc40a426f9d31479555c2eed3c7ffeb49ab6
sanjoyxd <sanjoyxd@ymail.com>
2021-07-01 01:24:44 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,8 +26,8 @@ const Footer = () => {
               return (
                 <a
                   key={id}
-                  href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
-                  rel="noopener noreferrer"
+                  href={url || 'https://dev-sanjoymixstudio.pantheonsite.io/'}
+                  rel=" "
                   target="_blank"
                   aria-label={name}
                 >
@@ -38,9 +38,9 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
+          © {new Date().getFullYear()} - Powered by{' '}
+          <a href="https://dev-sanjoymixstudio.pantheonsite.io/" target="_blank" rel=" ">
+            Sanjoy Choudhury
           </a>
         </p>
 
